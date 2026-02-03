839c930078d285b73d3570f17fc2d1bbef523c3c
bosssriyan <bosssriyan05@gmail.com>
2026-02-04 00:11:24 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import gzipPlugin from "rollup-plugin-gzip";
-
 import dotenv from "dotenv";
+
 dotenv.config();
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio_site/",
   build: {
-    // ...
     rollupOptions: {
-      // ...
       plugins: [
-        // ...
         gzipPlugin({
-          // Wähle die Dateitypen aus, die du komprimieren möchtest
           filter: /\.(js|css|html|json|svg)$/,
-          // Gzip-Komprimierungseinstellungen (optional)
-          minSize: 1024, // Mindestgröße der komprimierten Datei in Bytes
+          minSize: 1024,
         }),
       ],
     },
